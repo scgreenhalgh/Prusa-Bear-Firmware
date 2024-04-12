1df4a6384b705d08a94c0fad50147e9213d067f7
scgreenhalgh <me@seangreenhalgh.com>
2024-04-12 21:21:09 +0800

    Update TMC2130 currents for homing and running axes - increased extruder

--- Firmware/variants/MK3S.h
+++ Firmware/variants/MK3S.h
@@ -300,9 +300,15 @@
 // #define TMC2130_CURRENTS_R_HOME {13, 15, 25, 23}  // homing running currents for all axes
 
 // Above homing was a little low
-#define TMC2130_CURRENTS_H {21, 25, 40, 35}  // default holding currents for all axes
-#define TMC2130_CURRENTS_FARM 41             // E 805 mA peak for ECool/farm mode
-#define TMC2130_CURRENTS_R {21, 25, 40, 35}  // default running currents for all axes
+// #define TMC2130_CURRENTS_H {21, 25, 40, 35}  // default holding currents for all axes
+// #define TMC2130_CURRENTS_FARM 41             // E 805 mA peak for ECool/farm mode
+// #define TMC2130_CURRENTS_R {21, 25, 40, 35}  // default running currents for all axes
+// #define TMC2130_CURRENTS_R_HOME {16, 25, 35, 35}  // homing running currents for all axes
+
+// Lower Holding currents
+#define TMC2130_CURRENTS_H {16, 25, 35, 35}  // default holding currents for all axes
+#define TMC2130_CURRENTS_FARM 36             // E 805 mA peak for ECool/farm mode
+#define TMC2130_CURRENTS_R {21, 25, 40, 40}  // default running currents for all axes
 #define TMC2130_CURRENTS_R_HOME {16, 25, 35, 35}  // homing running currents for all axes
 
 // Just buping up Homing
