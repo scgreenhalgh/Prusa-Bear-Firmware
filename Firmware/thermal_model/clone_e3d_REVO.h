ba9c89c030c0c0b375fd78e9968001d72c23c78b
scgreenhalgh <me@seangreenhalgh.com>
2024-11-15 23:58:16 +0800

    Update thermal model parameters for E3D REVO

--- Firmware/thermal_model/clone_e3d_REVO.h
+++ Firmware/thermal_model/clone_e3d_REVO.h
@@ -6,8 +6,8 @@
 #define THERMAL_MODEL_E3D_REVO_U -0.0014  // linear temperature coefficient (W/K/power)
 #define THERMAL_MODEL_E3D_REVO_V 1.05     // linear temperature intercept (W/power)
 
-#define THERMAL_MODEL_E3D_REVO_C 8.77     // initial guess for heatblock capacitance (J/K)
-#define THERMAL_MODEL_E3D_REVO_R 25.3     // initial guess for heatblock resistance (K/W)
+#define THERMAL_MODEL_E3D_REVO_C 11.76     // initial guess for heatblock capacitance (J/K)
+#define THERMAL_MODEL_E3D_REVO_R 16.95     // initial guess for heatblock resistance (K/W)
 
 #define THERMAL_MODEL_E3D_REVO_fS 0.15    // sim. 1st order IIR filter factor (f=100/27)
 #define THERMAL_MODEL_E3D_REVO_LAG 270    // sim. response lag (ms, 0-2160)
@@ -16,7 +16,7 @@
 #define THERMAL_MODEL_E3D_REVO_E 1.23     // Default error threshold (K/s)
 
 // fall-back resistance vector (R0-15)
-#define THERMAL_MODEL_E3D_REVO_Rv {THERMAL_MODEL_E3D_REVO_R, 23.9, 22.5, 19.6, 19.0, 18.3, 17.7, 17.1, 16.8, 16.5, 16.3, 16.0, 15.9, 15.7, 15.6, 15.4}
+#define THERMAL_MODEL_E3D_REVO_Rv {THERMAL_MODEL_E3D_REVO_R, 16.22, 15.49, 14.03, 13.62, 13.21, 12.80, 12.38, 12.18, 11.97, 11.77, 11.56, 11.46, 11.35, 11.25, 11.15}
 
 // Send: M301
 // Recv: ok p:40.98 i:7.99 d:52.56
