51cc2bb25671a33e3ce5ad124d38a8f2ed40d808
scgreenhalgh <me@seangreenhalgh.com>
2024-11-15 18:18:58 +0800

    Update thermal model parameters for E3D REVO

--- Firmware/thermal_model/clone_e3d_REVO.h
+++ Firmware/thermal_model/clone_e3d_REVO.h
@@ -17,3 +17,43 @@
 
 // fall-back resistance vector (R0-15)
 #define THERMAL_MODEL_E3D_REVO_Rv {THERMAL_MODEL_E3D_REVO_R, 23.9, 22.5, 19.6, 19.0, 18.3, 17.7, 17.1, 16.8, 16.5, 16.3, 16.0, 15.9, 15.7, 15.6, 15.4}
+
+// OLD
+// Recv: echo:Thermal Model settings:
+// Recv: echo:  M310 I0 R12.98
+// Recv: echo:  M310 I1 R12.21
+// Recv: echo:  M310 I2 R11.45
+// Recv: echo:  M310 I3 R9.93
+// Recv: echo:  M310 I4 R9.60
+// Recv: echo:  M310 I5 R9.26
+// Recv: echo:  M310 I6 R8.93
+// Recv: echo:  M310 I7 R8.60
+// Recv: echo:  M310 I8 R8.46
+// Recv: echo:  M310 I9 R8.32
+// Recv: echo:  M310 I10 R8.18
+// Recv: echo:  M310 I11 R8.05
+// Recv: echo:  M310 I12 R7.95
+// Recv: echo:  M310 I13 R7.86
+// Recv: echo:  M310 I14 R7.76
+// Recv: echo:  M310 I15 R7.67
+// Recv: echo:  M310 P38.00 U0.0000 V1.00 C12.28 D0.0650 L2160 S1 B1 E1.74 W1.20 T-7.00
+
+// NEW
+// Recv: echo:Thermal Model settings:
+// Recv: echo:  M310 I0 R12.98
+// Recv: echo:  M310 I1 R12.39
+// Recv: echo:  M310 I2 R11.81
+// Recv: echo:  M310 I3 R10.65
+// Recv: echo:  M310 I4 R10.33
+// Recv: echo:  M310 I5 R10.01
+// Recv: echo:  M310 I6 R9.70
+// Recv: echo:  M310 I7 R9.38
+// Recv: echo:  M310 I8 R9.24
+// Recv: echo:  M310 I9 R9.11
+// Recv: echo:  M310 I10 R8.97
+// Recv: echo:  M310 I11 R8.83
+// Recv: echo:  M310 I12 R8.75
+// Recv: echo:  M310 I13 R8.66
+// Recv: echo:  M310 I14 R8.58
+// Recv: echo:  M310 I15 R8.49
+// Recv: echo:  M310 P38.00 U0.0000 V1.00 C12.28 D0.0650 L2160 S1 B1 E1.74 W1.20 T-7.00
