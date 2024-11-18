d02e2d1310ca722b0a1bdb98ef44c68a5db38a80
scgreenhalgh <me@seangreenhalgh.com>
2024-11-18 18:46:32 +0800

    Update fan check thresholds in MK3S-CLONE-E3DREVO.h

--- Firmware/variants/MK3S-CLONE-E3DREVO.h
+++ Firmware/variants/MK3S-CLONE-E3DREVO.h
@@ -355,8 +355,8 @@
 #define EXTRUDER_ALTFAN_DETECT
 #define EXTRUDER_ALTFAN_SPEED_SILENT 128
 
-#define FANCHECK_AUTO_PRINT_FAN_THRS 50 //[RPS] - Used during selftest to identify swapped fans automatically
-#define FANCHECK_AUTO_FAIL_THRS 10 //[RPS] - Used during selftest to identify a faulty fan
+#define FANCHECK_AUTO_PRINT_FAN_THRS 70 //[RPS] - Used during selftest to identify swapped fans automatically
+#define FANCHECK_AUTO_FAIL_THRS 20 //[RPS] - Used during selftest to identify a faulty fan
 
 /*------------------------------------
  CHANGE FILAMENT SETTINGS
