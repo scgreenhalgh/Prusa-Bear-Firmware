b486109d13ade6857d0a045a45df8018fbd2d912
scgreenhalgh <me@seangreenhalgh.com>
2024-11-20 17:11:14 +0800

    Uncomment additional custom character mappings in FontGen.py for proper character representation

--- lang/lib/FontGen.py
+++ lang/lib/FontGen.py
@@ -133,11 +133,11 @@ FONT_TABLE = [
 BUILTIN_CHARS = {
     '\xc7': '→',
     '\xc8': '←',
-    # '\xe1': 'ä',
-    # '\xe4': 'µ', #on keyboard AltGr+m it is \xC2\xB5
-    # '\xef': 'ö',
-    # '\xf5': 'ü',
-    # '\xff': '█', # not in the european LCD
+    '\xe1': 'ä',
+    '\xe4': 'µ', #on keyboard AltGr+m it is \xC2\xB5
+    '\xef': 'ö',
+    '\xf5': 'ü',
+    '\xff': '█', # not in the european LCD
 }
 
 # Mapping from LCD source encoding to unicode characters
