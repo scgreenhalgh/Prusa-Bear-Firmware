5132cf63f3145418af122835cc588decd589e30a
scgreenhalgh <me@seangreenhalgh.com>
2024-11-20 16:25:44 +0800

    Add debug print statement for custom character mapping in FontGen.py

--- lang/lib/FontGen.py
+++ lang/lib/FontGen.py
@@ -150,6 +150,7 @@ for index in range(len(FONT_TABLE)):
     else:
         CUSTOM_CHARS.update({chr(index + 0x80): FONT_TABLE[custom_char_index].utf})
         custom_char_index += 1
+    print(f"Custom character {FONT_TABLE[index].utf} at index 0x{index + 0x80:02X}")
 CUSTOM_CHARS.update(BUILTIN_CHARS)
 
 INVERSE_CUSTOM_CHARS = {v: k for k, v in CUSTOM_CHARS.items()}
