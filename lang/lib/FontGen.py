d6631d6107f13329a7c0feb99cde1f7b032e5e66
scgreenhalgh <me@seangreenhalgh.com>
2024-11-20 17:38:44 +0800

    Fix custom character index increment logic in FontGen.py to correctly handle builtin character skipping

--- lang/lib/FontGen.py
+++ lang/lib/FontGen.py
@@ -147,8 +147,8 @@ custom_chars_index = 0
 for index in range(len(FONT_TABLE)):
     char = chr(index + 0x80)
     if char in BUILTIN_CHARS:
-        custom_chars_index += 1
         print(f"Skipping address for builtin char '{custom_chars_index + 0x80:#x}'")
+        custom_chars_index += 1
     CUSTOM_CHARS.update({chr(custom_chars_index + 0x80): FONT_TABLE[index].utf})
     print(f"Adding custom char '{FONT_TABLE[index].utf}' at '{custom_chars_index + 0x80:#x}'")
     custom_chars_index += 1
